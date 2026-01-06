152a056eb137f6671493d71caa4dcdd82d317f0a
AnushkaPriya <anushkapriya.me@gmail.com>
2026-01-06 21:25:40 +0530

    added

---
+++ 2011.java
@@ -0,0 +1,16 @@
+//Q2011. Final Value of Variable After Performing Operations
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+
+        for (int i = 0; i < operations.length; i++) {
+            if (operations[i].contains("--")) {
+                x--;
+            } else {
+                x++;
+            }
+        }
+
+        return x;
+    }
+}
